2fc375e363922cf4788f7f5cf50e7e18da2df2ee
Frankie <1426203851@qq.com>
2026-02-11 02:48:23 +0800

    feat: update ESLint Stylistic rules

--- packages/eslint/src/config.ts
+++ packages/eslint/src/config.ts
@@ -16,6 +16,18 @@ export function defineConfig(options: Options, userConfigs: UserConfigs): Config
       // base: https://eslint.org/docs/latest/rules/
       'no-console': 'off',
       'no-debugger': 'warn',
+      'no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'all',
+          argsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+          // TODO: 为什么 const { foo: _bar } = baz 的 _bar 会被识别为未使用变量
+        },
+      ],
 
       // eslint-plugin-n: https://github.com/eslint-community/eslint-plugin-n
       'node/prefer-global/process': 'off',
@@ -26,6 +38,24 @@ export function defineConfig(options: Options, userConfigs: UserConfigs): Config
 
       // pnpm: https://github.com/antfu/pnpm-workspace-utils/tree/main/packages/eslint-plugin-pnpm
       'pnpm/yaml-enforce-settings': 'off',
+
+      // ESLint Stylistic: https://eslint.style/rules/quotes#single
+      'style/quotes': ['error', 'single', { avoidEscape: true }],
+      'style/arrow-parens': ['error', 'as-needed'],
+      'style/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+      'style/operator-linebreak': [
+        'error',
+        'before',
+        {
+          overrides: {
+            '&&': 'after',
+          },
+        },
+      ],
+      'style/comma-dangle': 'off', // 交由 prettier 处理
+
+      // eslint-plugin-eslint-comments: https://mysticatea.github.io/eslint-plugin-eslint-comments/
+      'eslint-comments/no-unlimited-disable': 'off',
     },
   }
 
