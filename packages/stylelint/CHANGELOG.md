a5a47247dc402791478c83dddd65da505775c06c
Frankie <1426203851@qq.com>
2026-02-23 02:46:02 +0800

    feat: add declaration-property-value-no-unknown rule

--- packages/stylelint/CHANGELOG.md
+++ packages/stylelint/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## stylelint@0.0.3 (2026-02-23)
+
+- Add `declaration-property-value-no-unknown` rule for miniapp
+
 ## stylelint@0.0.2 (2026-02-20)
 
 - Update `exports` field for CommonJS compatibility
--- packages/stylelint/src/miniapp.ts
+++ packages/stylelint/src/miniapp.ts
@@ -5,5 +5,21 @@ export default {
     // https://stylelint.io/user-guide/rules/unit-no-unknown/#ignoreunits
     'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
     'selector-type-no-unknown': [true, { ignoreTypes: ['page'] }],
+    // https://stylelint.io/user-guide/rules/declaration-property-value-no-unknown/
+    // https://github.com/stylelint/stylelint/issues/9037#issuecomment-3891721635
+    'declaration-property-value-no-unknown': [
+      true,
+      {
+        ignoreProperties: {
+          '/.+/': ['/[0-9]+(\\.[0-9]+)?rpx/'], // 避免 font-size: 10rpx、10.5rpx 此类写法被误报
+        },
+      },
+    ],
   },
+  overrides: [
+    {
+      files: ['*.wxss'],
+      customSyntax: 'postcss',
+    },
+  ],
 } satisfies Config
