71c35cc46d2410e978eecdee653aa1d5c35bd586
Frankie <1426203851@qq.com>
2026-04-17 08:16:56 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from '@tofrankie/eslint'
 
 export default defineConfig({
   ignores: ['**/*.md', 'packages/eslint/tests/**', 'packages/tsconfig/base/**/*.json'],
-  vue: true,
-  pnpm: true,
-  jsdoc: false,
-  rules: {
-    'e18e/prefer-static-regex': 'off',
-  },
+  typescript: true,
 })
